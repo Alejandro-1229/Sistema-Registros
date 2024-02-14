160f3053d05894d139852c28ec8ca1227466a624
cesar1229 <6275030@gmail.com>
2024-02-13 23:33:49 -0500

    Avance en la creacion de los archivos necesarios

---
+++ app/Http/Controllers/NivelRiesgoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Nivel_Riesgo;
+use Illuminate\Http\Request;
+
+class NivelRiesgoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Nivel_Riesgo $nivel_Riesgo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Nivel_Riesgo $nivel_Riesgo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Nivel_Riesgo $nivel_Riesgo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Nivel_Riesgo $nivel_Riesgo)
+    {
+        //
+    }
+}
